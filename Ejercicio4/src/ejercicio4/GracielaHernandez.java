9b9d43ca3c4bd2bbefe0beb48da8e1a979ab77dc
graciehernandez22 <graciehernandez022@gmail.com>
2022-05-06 14:33:05 -0600

    aporte

---
+++ Ejercicio4/src/ejercicio4/GracielaHernandez.java
@@ -0,0 +1,12 @@
+
+package ejercicio4;
+
+
+public class GracielaHernandez {
+     public static void main(String[] args) {
+        double resta;
+        
+        resta = 1-1;
+        System.out.println("La Resta de 1 - 1 es: " + resta);
+    } 
+}

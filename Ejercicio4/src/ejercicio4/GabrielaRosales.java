1cf1c13db7d60e79da5dac085093069323abcf21
gabirosales <gabrielarosales169@gmail.com>
2022-05-06 14:28:37 -0600

    clase Gabriela

---
+++ Ejercicio4/src/ejercicio4/GabrielaRosales.java
@@ -0,0 +1,12 @@
+
+package ejercicio4;
+
+
+public class GabrielaRosales {
+    public static void main(String[] args) {
+        String apellido;
+        
+        apellido = "Rosales";
+        System.out.println("Apellido" + apellido);
+    }
+}
